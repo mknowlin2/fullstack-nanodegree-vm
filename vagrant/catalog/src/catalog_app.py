acb4ae84cbb6d3dd3906edac60b758c953d8811c
Michael A. Knowlin II <mknowlin2@gmail.com>
2017-11-24 18:48:49 -0500

    Initial draft.

---
+++ vagrant/catalog/src/catalog_app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+# The Catalog Web application.
+from flask import Flask, jsonify, request, url_for, abort
+from database.data_access import get_users, get_user_by_username, add_user
+
+
+app = Flask(__name__)
+
+
+@app.route('/user', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    print (username)
+
+    if username is None:
+        abort(400) # missing arguments
+
+    user = get_user_by_username(username)
+
+    if user is not None:
+        print('Existing Username')
+        return jsonify({'message':'User already exists'}), 200
+
+    add_user(username)
+    return jsonify({'message': 'New user added'}), 201
+
+
+if __name__ == '__main__':
+    app.secret_key = 'dev_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
